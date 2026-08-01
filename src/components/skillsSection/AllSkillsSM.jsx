0e28a9f5107a92692c1becea72fadaa54de7ddc7
Nihar Patel <niharpatel2545@gmail.com>
2026-08-01 15:47:11 +1000

    mobile changes

--- src/components/skillsSection/AllSkillsSM.jsx
+++ src/components/skillsSection/AllSkillsSM.jsx
@@ -2,14 +2,14 @@ import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import { FaReact } from "react-icons/fa";
-import { FaJava } from "react-icons/fa";
 import { FaPython } from "react-icons/fa";
 import { FaNode } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
-import { SiScratch } from "react-icons/si";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
+import { SiNumpy, SiPandas } from "react-icons/si";
+import { FaChartBar } from "react-icons/fa"; 
 
 const skills = [
   {
@@ -45,12 +45,16 @@ const skills = [
     icon: FaPython,
   },
   {
-    skill: "Java",
-    icon: FaJava,
+    skill: "NumPy",
+    icon: SiNumpy,
   },
   {
-    skill: "Scratch",
-    icon: SiScratch,
+    skill: "Pandas",
+    icon: SiPandas,
+  },
+  {
+    skill: "Seaborn",
+    icon: FaChartBar,
   },
 ];
 
